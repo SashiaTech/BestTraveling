6115ad3f6300a9c43ec3218ac58f5753f2e2465e
Avtar <avtarsashia90564@gmail.com>
2020-05-11 08:15:31 +0530

    Add project files.

---
+++ BestTraveling/Areas/College/CollegeAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace BestTraveling.Areas.College
+{
+    public class CollegeAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "College";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "College_default",
+                "College/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ BestTraveling/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BestTraveling.MvcApplication" Language="C#" %>

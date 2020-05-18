7024ac7300ff1b71f100b87794c361d744dfbb81
Avtar <avtarsashia90564@gmail.com>
2020-05-18 12:14:23 +0530

    Added repository

---
+++ BT College Repository/IRepository/ICollegeRepo.cs
@@ -0,0 +1,15 @@
+﻿using BT_Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BT_College_Repository.IRepository
+{
+    public interface ICollegeRepo
+    {
+        string Create();
+    }
+}
---
+++ BT College Repository/Repository/CollegeRepo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BT_College_Repository.IRepository;
+using BT_Model;
+
+namespace BT_College_Repository.Repository
+{
+   public class CollegeRepo : ICollegeRepo
+    {
+        public string Create()
+        {
+            return "Create Test";
+        }
+    }
+}

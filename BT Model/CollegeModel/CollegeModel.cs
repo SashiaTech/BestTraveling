b418308afa2e13e1fe0e02be681b392c6ee5cdfa
Avtar <avtarsashia90564@gmail.com>
2020-05-18 12:15:58 +0530

    added files

---
+++ BT Model/CollegeModel/CollegeModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT_Model
+{
+    public class CollegeModel
+    {
+        public Guid CollegeId { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string District { get; set; }
+        public string DirectorName { get; set; }
+        public string Contact { get; set; }
+        public string Email { get; set; }
+    }
+}

ec8f5dae14f37bda1cde80624cec667a4c8974ca
Avtar <avtarsashia90564@gmail.com>
2020-05-18 12:11:34 +0530

    Added missing controller

---
+++ BestTraveling/Areas/College/Controllers/CollegeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using bt_Services.IServices;
+using BT_Model;
+
+namespace BestTraveling.Areas.College.Controllers
+{
+    public class CollegeController : Controller
+    {
+        ICollegeService _collegeService = null;
+
+        public CollegeController(ICollegeService _collegeService)
+        {
+            this._collegeService = _collegeService;
+        }
+
+        // GET: College/College
+        public ActionResult Create()
+        {
+            CollegeModel model = new CollegeModel();
+            model.Name = _collegeService.Create();
+            return View(model);
+        }
+        
+        public ActionResult index()
+        {
+            return View();
+        }
+    }
+}

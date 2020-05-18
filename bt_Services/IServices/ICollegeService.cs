4ef0a9377b3e99cba5021b63883d1af3edeb1773
Avtar <avtarsashia90564@gmail.com>
2020-05-18 12:16:40 +0530

    added files

---
+++ bt_Services/IServices/ICollegeService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BT_Model;
+
+namespace bt_Services.IServices
+{
+    public interface ICollegeService
+    {
+        string Create();
+    }
+}
---
+++ bt_Services/Services/CollegeService.cs
@@ -0,0 +1,28 @@
+﻿using BT_Model;
+using bt_Services.IServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BT_College_Repository;
+using BT_College_Repository.IRepository;
+using BT_College_Repository.Repository;
+
+namespace bt_Services.Services
+{
+    public class CollegeService : ICollegeService
+    {
+        ICollegeRepo _collegeRepo = null;
+
+        public CollegeService()
+        {
+            _collegeRepo = new CollegeRepo();
+        }
+
+        public string Create()
+        {
+            return _collegeRepo.Create();
+        }
+    }
+}

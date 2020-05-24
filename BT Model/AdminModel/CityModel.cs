0a29b7d6be20117a1b6301682206b3443946d525
Avtar <avtarsashia90564@gmail.com>
2020-05-24 10:12:16 +0530

    city model added

---
+++ BT Model/AdminModel/CityModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT_Model
+{
+    public class CityModel
+    {
+        public Guid CityId { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public Guid? DistrictId{ get; set; }
+        
+    }
+}

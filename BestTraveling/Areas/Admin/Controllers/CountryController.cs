5163220bad47c3e5a469c02134f555651b0a1c5e
Manpreet-singh <ms4110415@gmail.com>
2020-08-05 09:05:35 +0530

    Worked on Country State and City By Mnprt

---
+++ BestTraveling/Areas/Admin/Controllers/CountryController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BestTraveling.Areas.Admin.Controllers
+{
+    public class CountryController : Controller
+    {
+        // GET: Admin/Country
+        public ActionResult List()
+        {
+            return View();
+        }
+    }
+}
---
+++ BestTraveling/Areas/Admin/Controllers/StateController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BestTraveling.Areas.Admin.Controllers
+{
+    public class StateController : Controller
+    {
+        // GET: Admin/State
+        public ActionResult List()
+        {
+            return View();
+        }
+    }
+}

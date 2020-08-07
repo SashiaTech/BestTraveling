99c516dfc84a8757f44a10d24430e9ee68a8ad70
Avtar <avtarsashia90564@gmail.com>
2020-08-07 07:49:52 +0530

    Added Role controller

---
+++ BestTraveling/Areas/Admin/Controllers/RoleController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BestTraveling.Areas.Admin.Controllers
+{
+    public class RoleController : Controller
+    {
+        // GET: Admin/Role
+        public ActionResult List()
+        {
+            return View();
+        }
+        public ActionResult Add()
+        {
+            return View();
+        }
+    }
+}

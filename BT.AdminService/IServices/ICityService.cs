e428ed34df4f4b1a8182d1a5e6a5ad2f97723145
Avtar <avtarsashia90564@gmail.com>
2020-08-08 17:10:10 +0530

    Added pending files

---
+++ BT.AdminService/IServices/ICityService.cs
@@ -0,0 +1,20 @@
+﻿//ICityService
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BT_Model;
+
+namespace BT.AdminService.IServices
+{
+    public interface ICityService
+    {
+        IQueryable<CityModel> GetCitis();
+        CityModel GetCityById(Guid CityId);
+        void AddCity(CityModel model);
+        void UpdateCity(CityModel model);
+        void RemoveCity(CityModel model);
+    }
+}
---
+++ BT.AdminService/Services/CityService.cs
@@ -0,0 +1,48 @@
+﻿//CityService
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BT.AdminService.IServices;
+using BT_Model;
+using BT.AdminRepository.IRepository;
+using BT.AdminRepository.Repository;
+
+
+namespace BT.AdminService.Services
+{
+    public class CityService : ICityService
+    {
+        private readonly ICityRepo _ICityRepo = null;
+        public CityService()
+        {
+            this._ICityRepo = new CityRepo();
+        }
+        public void AddCity(CityModel model)
+        {
+            _ICityRepo.AddCity(model);
+        }
+
+        public IQueryable<CityModel> GetCitis()
+        {
+            return _ICityRepo.GetCitis();
+        }
+
+        public CityModel GetCityById(Guid CityId)
+        {
+            return _ICityRepo.GetCityById(CityId);
+        }
+
+        public void RemoveCity(CityModel model)
+        {
+            _ICityRepo.RemoveCity(model);
+        }
+
+        public void UpdateCity(CityModel model)
+        {
+            _ICityRepo.UpdateCity(model);
+        }
+    }
+}

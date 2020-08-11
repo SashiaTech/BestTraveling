8b937a1bc640d5a45ee30bb64e232ff1863db952
Avtar <avtarsashia90564@gmail.com>
2020-08-11 08:22:51 +0530

    Added UserScreen Controller and Model

---
+++ BT Model/AdminModel/UserModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT_Model.AdminModel
+{
+    public class UserModel
+    {
+        public Guid UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Code { get; set; }
+        public string Contact { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ BestTraveling/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BestTraveling.Areas.Admin.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: Admin/User
+        public ActionResult List()
+        {
+            return View();
+        }
+        public ActionResult Add()
+        {
+            return View();
+        }
+    }
+}

9c2e5a5803655593444826bce914d0ab057206fd
Avtar <avtarsashia90564@gmail.com>
2020-08-13 09:31:42 +0530

    Added StudentAuthorization Controller and views

---
+++ BT Model/CollegeModel/StudentAuthorizationModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT_Model.CollegeModel
+{
+    public class StudentAuthorizationModel
+    {
+        public Guid AuthorizationId { get; set; }
+        public Guid StudentId { get; set; }
+        public Guid CollegeId { get; set; }
+        public string CollegeName { get; set; }
+
+    }
+}
---
+++ BestTraveling/Areas/College/Controllers/StudentAuthorizationController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BestTraveling.Areas.College.Controllers
+{
+    public class StudentAuthorizationController : Controller
+    {
+        // GET: College/StudentAuthorization
+        public ActionResult List()
+        {
+            return View();
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+    }
+}

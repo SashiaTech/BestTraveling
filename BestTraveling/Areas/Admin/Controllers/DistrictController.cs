86566893ad3ad877fde50c266ecd18cb5a1657c6
Avtar <avtarsashia90564@gmail.com>
2020-08-15 19:15:51 +0530

    Added District controller

---
+++ BestTraveling/Areas/Admin/Controllers/DistrictController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BestTraveling.Areas.Admin.Controllers
+{
+    public class DistrictController : Controller
+    {
+        // GET: Admin/District
+        public ActionResult List()
+        {
+            return View();
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+    }
+}

27b5977b495c6dded732a7d17fd1770ea51c43af
ramgill <rammoudgill3054@outlook.com>
2020-08-20 11:57:51 +0530

    side bar improvements

--- BestTraveling/Content/UIContentFiles/js/index.js
+++ BestTraveling/Content/UIContentFiles/js/index.js
@@ -1,23 +1,44 @@
-﻿const ul = document.querySelector('#ul');
+﻿const toggle = document.querySelector('#toggle');
+const ul_list = document.querySelector('#ul');
 const sideBarIcons = document.querySelector(".sideBarIcons");
-const toggle = document.querySelector('#toggle');
-toggle.addEventListener('click', () => {
-    const menuBar = document.querySelector('#menuBar');
-    if (menuBar.style.left === "-1px") {
-        menuBar.style.left = "0";
-        //toggle.style.left = "250px";
-        ul.style.display = "block";
-        sideBarIcons.style.display = "none";
+window.addEventListener("resize", () => {
+    let windowSize = window.innerWidth;
+    if (windowSize > 768) {
         menuBar.style.width = "300px";
+        ul_list.style.display = "block";
+        sideBarIcons.style.display = "none";
+    }
+});
+toggle.addEventListener('click', toggler);
+
+function toggler(e) {
+    const menuBar = document.querySelector('#menuBar');
+    const query = window.matchMedia("(max-width: 768px)")
+    if (query.matches) {
+        if (menuBar.style.width === "300px") {
+            menuBar.style.width = "0px";
+            ul_list.style.display = "none";
+            sideBarIcons.style.display = "none";
+        }
+        else {
+            menuBar.classList.remove("win");
+            menuBar.style.width = "300px";
+            ul_list.style.display = "block";
+            sideBarIcons.style.display = "none";
+        }
+    }
+    else if (menuBar.style.width === "45px") {
+        menuBar.style.width = "300px";
+        ul_list.style.display = "block";
+        sideBarIcons.style.display = "none";
     }
     else {
-        menuBar.style.left = "-1px";
-        //toggle.style.left = "150px";
-        ul.style.display = "none";
+        menuBar.style.width = "45px";
+        ul_list.style.display = "none";
         sideBarIcons.style.display = "block";
-        menuBar.style.width = "70px";
     }
-})
+}
+
 let anchor_Design = document.getElementsByClassName('anchor-design');
 Array.from(anchor_Design).forEach((Element, index) => {
     let rytArrowBtn = document.getElementsByClassName('rytArrowBtn');
@@ -36,7 +57,7 @@ Array.from(anchor_Design).forEach((Element, index) => {
         else {
             submenu[index].style.maxHeight = "0px";
             rytArrowBtn[index].className = "fa fa-angle-right rytArrowBtn";
-            // submenu[index].style.overflow = "hidden";
+            
         }
     });
 });

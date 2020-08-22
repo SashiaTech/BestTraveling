e4f086fa2b0ff689f91c024edad37e9988cb80a8
Avtar <avtarsashia90564@gmail.com>
2020-08-22 09:17:28 +0530

    Added Common Controller files

---
+++ BestTraveling/Controllers/CommonDataController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BestTraveling.Controllers
+{
+    public class CommonDataController : Controller
+    {
+        // GET: CommonData
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

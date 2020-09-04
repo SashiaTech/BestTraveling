347339489c71f957f23e1ef5b1dded46f13275e4
ramgill <rammoudgill3054@outlook.com>
2020-09-04 07:26:44 +0530

    office controller and views added

---
+++ BestTraveling/Areas/Admin/Controllers/OfficeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BestTraveling.Areas.Admin.Controllers
+{
+    public class OfficeController : Controller
+    {
+        // GET: Admin/Office
+        public ActionResult AddOffice()
+        {
+            return View();
+        }
+        public ActionResult list()
+        {
+            return View();
+        }
+    }
+}
